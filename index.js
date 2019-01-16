0807a99a60bfbb90528b9b045fd7922ade73a00f
nise <niels.seidel@nise81.com>
2019-01-16 16:09:35 +0100

    process multiple tex files

--- index.js
+++ index.js
@@ -11,14 +11,19 @@
 
 const
     p = require('./tex2SSML'),
-    path = '/home/abb/Documents/proj_001_doc/teaching-courses/2017-Gestaltung-kooperativer-Systeme/cvs/Kurs1884/Kurstext/',
+    path = '/home/abb/Documents/proj_001_doc/teaching-courses/2017-Gestaltung-kooperativer-Systeme/cvs/Kurs1884/Kurstext/polly/',
     path2 = 'tex/',
-    file = 'ke7.tex'//ke6-utf8.tex
+    file = 'ke2.tex'//ke6-utf8.tex
     ;
-p.tex2SSML(path2 + file);
+p.tex2SSML(file, path);
 
+//for (var i = 1; i < 8; i++) { p.tex2SSML('ke'+i+'.tex', path); }
 
-
+var t = 'bst aus, wobei nicht immer explizit in Form ein er Ich - Botschaft gesprochen werden muss.\textquote{ In jeder Nachricht steckt ein Stück Selbstoffenbarung des Senders.Ich wähle den Begriff der Selbstoffenbarung, um damit sowohl die gewollte \emph{ Selbstdarstellung } als auch die unfreiwillige \emph{ Selbstenthüllung } einzuschließen } (ebd.).Beim Lesen dieser Kurseinheit erfahren Sie-- wie schon bei den vorangegangenen Kurseinheiten-- eine Menge über den Autor dieses Kurses.Zwischen den Zeilen lesen Sie vielleicht heraus, dass der Autor das Thema der kooperativen Systeme gerade nicht nur aus technischer Perspektive betrach'
+var handleQuote = function (match, capture) {
+    return capture;
+}
+//console.log(t  .replace(/emph\{([^\0]*?)\}/g, '$1').replace(/extquote\{([^\0]*?)\}/g, handleQuote));
 
 /**
  *  Performs the Google Text-to-Speech request
