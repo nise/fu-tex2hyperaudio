bff873ba4f0f2c977f0c8f73fee75c2e9131e027
nise <niels.seidel@nise81.com>
2019-01-28 23:52:14 +0100

    refactoring: separted text analysis from experiment

--- textanalysis.js
+++ textanalysis.js
@@ -1,42 +1,50 @@
 /**
  * 
+ * author: niels.seidel@fernuni-hagen.de
  * Source: https://web.archive.org/web/20160712094308/http://www.mang.canterbury.ac.nz/writing_guide/writing/flesch.shtml
  */
 
 const
     fs = require('fs'),
-    debug = false // debug modus on/off
+    debug = false, // debug modus on/off
+    echo = false
     ;
 let
-    sentences = [] // Array of sentences
+    sentences = [],
+    ssml=''
     ;
-exports.sentences = sentences;
 
 /**
  * Preprocessing 
  * TODO/Flesh requirements: Test only the running text of your piece of writing. Skip titles, headings, subheads, section and paragraph numbers, captions, date lines and signature lines.
  * @param {String} text
- * @param {String} file
+ * @param {String} document
  */
-exports.analyse = function (text, file) {
+exports.analyse = function (text, document) {
+    if(text === null){
+        return;
+    }
+    ssml = text;
+    text_id = document;
     // strip tags
     text = text.replace(/(<([^>]+)>)/ig, "");
     // split sentences
     text = text.replace(/\\n/g, "\r");
-    var re = /\b(\w\.\ \w\.|\w\.\w\.|bzw\.|bspw\.|[0-9]+\.)|([.?!])\s+(?=[A-Za-z])/g;
+    var re = /\b(\w\.\ \w\.|\w\.\w\.|vgl\.|bzw\.|bspw\.|[0-9]+\.)|([.?!])\s+(?=[A-Za-z])/g;
     var result = text.replace(re, function (m, g1, g2) {
         return g1 ? g1 : g2 + "\r";
     });
     sentences = result.split("\r");
-    calculateFleshIndex(file);
+    calculateFleshIndex(document);
     //fs.writeFile('output/full-text.txt', text, err => {});
+    return calculateFleshIndex(document);
 };
 
 
 /**
  * Calculates the Flesh index per sentence for a given long (xml/html) text
  */
-var calculateFleshIndex = function (file) {
+var calculateFleshIndex = function (document) {
     var
         words = [],
         total_syllables = 0,
@@ -45,7 +53,6 @@ var calculateFleshIndex = function (file) {
     // calculate Flesh index per sentence
     var
         sum = 0,
-        csv = 'id,words,flesh\n',
         json = []
         ;
     for (var i = 0; i < sentences.length; i++) {
@@ -59,41 +66,22 @@ var calculateFleshIndex = function (file) {
         // total_words / sentences.length
         //EN:  206.835 - 1.015 * (total_words / sentences.length) - 84.6 * (total_syllables / total_words);
         var flesh_de = 180 - (1) - 58.5 * (total_syllables / total_words);
-        //console.log(flesh_de, total_syllables, sentences[i]);
-        csv += i + ',' + ',' + total_words + ',' + flesh_de + '\n';
-        json.push({ id: i, words: total_words, flesh: flesh_de, text: sentences[i] });
+        
+        json.push({ document: document, id: i, words: total_words, flesh: flesh_de, text: sentences[i], selected: false });
         sum += flesh_de;
         // debug
         if (flesh_de < 0 && debug) {
             console.log(flesh_de + ' ' + ' ' + sentences[i] + '\n\n');
         }
     }
+   
     // print output
-    console.log('Flesh readability index (DE-de) of ' + file + ' ' + (sum / sentences.length).toFixed(2));
-    fs.writeFile('output/text-analysis-' + file + '.csv', csv, err => { });
-
-    // Get example sentences for certain pecentils
-    getPercentils(json, [0.10, 0.5, 0.9]);
-};
-
-
-/**
- * 
- * @param {Array} Array of json objects
- * @param {Array} Array of percentils to given out
- */
-var getPercentils = function(json, percentils){
-    // sort by flesh
-    var sentences = json.sort(function (obj1, obj2) {
-        return obj1.flesh - obj2.flesh;
-    });
-    // print percentils
-    var l = sentences.length;
-    for(var i = 0; i < percentils.length;i++){
-        if (percentils[i] < 1 && percentils[i] > 0){
-            console.log((percentils[i] * 100) + '% Percentil: ' + sentences[Math.ceil(l * percentils[i])].text);    
-        }
+    if (echo) {
+        console.log('Flesh readability index (DE-de) of ' + document + ' ' + (sum / sentences.length).toFixed(2));
     }
+    fs.appendFile('output/text-analysis-all.csv', json2csv(json), err => { }); 
+
+    return json;
 };
 
 
@@ -134,4 +122,18 @@ var countSyllables = function (word) {
         }
     }
     return numVowels;
+};
+
+
+/**
+ * Util function to convert a given json structure in static csv data
+ * @param {*} json 
+ * @param {*} percentils 
+ */
+var json2csv = function (json) {
+    var csv = '';//'document,id,words,flesh,selectd\n';
+    for(var i=0; i<json.length;i++){
+        csv += json[i].document + ',' + json[i].id + ',' + json[i].words + ',' + json[i].flesh + ',' + json[i].selected + '\n';
+    }
+    return csv;
 };
